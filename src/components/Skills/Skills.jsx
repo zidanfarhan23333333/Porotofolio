7b6cf671445e526389f3d593ea3b0bb9059d856a
unknown <sfarkhan48@gmail.com>
2024-10-08 09:52:52 +0700

    add skill info card

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { useState } from "react";
 import "./Skills.css";
 import { SKILLS } from "../utils/data";
 import SkillCard from "./SkillCard/SkillCard";
+import SkillsInfoCard from "./SkillsInfoCard/SkillsInfoCard";
+
 const Skills = () => {
+  const [selectedSkill, setSelectedSkill] = useState(SKILLS[0]);
+
+  const handleSelectSkill = (data) => {
+    setSelectedSkill(data); // Use setSelectedSkill to update the selected skill
+  };
+
   return (
     <section className="skills-container">
       <h5>Kemampuan teknis</h5>
@@ -14,11 +22,20 @@ const Skills = () => {
               key={skill.title}
               iconUrl={skill.icon}
               title={skill.title}
+              isActive={selectedSkill.title === skill.title}
+              onClick={() => {
+                handleSelectSkill(skill);
+              }}
             />
           ))}
         </div>
 
-        <div className="skills-info"></div>
+        <div className="skills-info">
+          <SkillsInfoCard
+            heading={selectedSkill.title}
+            skills={selectedSkill.skills}
+          />
+        </div>
       </div>
     </section>
   );
